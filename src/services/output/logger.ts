09345e6b950766efc8d18fa6a64e5868ad5659a6
Einar Aglen <einar.aglen99@gmail.com>
2023-03-10 16:20:25 +0100

    Removed unused code

--- src/services/output/logger.ts
+++ src/services/output/logger.ts
@@ -1,20 +1,3 @@
-// const log = {
-//     info: (message: string) =>
-//       console.log("\x1b[1;34m%s\x1b[0m", "Info:  ", message),
-//     user: (message: string) =>
-//       console.log("\x1b[1;32m%s\x1b[0m", "User:  ", message),
-//     error: (message: string, error?: any) =>
-//       error
-//         ? console.log("\x1b[1;31m%s\x1b[0m", "Error: ", message, "\n", error)
-//         : console.log("\x1b[1;31m%s\x1b[0m", "Error: ", message),
-//         warn: (message: string, error?: any) =>
-//         error
-//           ? console.log("\x1b[1;33m%s\x1b[0m", "Warn:  ", message, "\n", error)
-//           : console.log("\x1b[1;33m%s\x1b[0m", "Warn:  ", message),
-//   };
-  
-//   export default log;
-
 export default class Logger {
 
     private static time() {
